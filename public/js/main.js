c322a5e73976930f6216a541ccd5d44e7be00207
xiaoma <670567137@qq.com>
2020-05-23 15:20:45 +0800

    react

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-console.log($)
-
-$.get('./login',{ name:"Donald", town:"Ducktown" },function(res){
-    console.log(res);
-})
-
-$.post('./reg',{ name:"Donald", town:"Ducktown" },function(res){
-    console.log(res);
-})
---
+++ public/static/js/5.4ba548f7.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonpmy-app"]=window["webpackJsonpmy-app"]||[]).push([[5],{27:function(t,e,n){"use strict";n.r(e);var a=n(7),c=n(0),r=n.n(c);e.default=function(t){var e=Object(c.useState)(100),n=Object(a.a)(e,2),u=n[0],i=n[1];return Object(c.useEffect)((function(){var t=setInterval((function(){u>0&&(i((function(t){return t-1})),clearInterval(t))}),100)}),[u]),r.a.createElement("div",null,u," - ",t.title)}}}]);
+//# sourceMappingURL=5.4ba548f7.chunk.js.map
