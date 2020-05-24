5f24839f9d45247b8e711543b547e075b3d20ea4
xiaoma <670567137@qq.com>
2020-05-24 21:50:06 +0800

    22

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose');
 // let url = 'mongodb://127.0.0.1:27017/local'
-// mongoose.connect(url,{
-//   user : 'root',
-//   pass : 'liuxiaoma123',
-//   useNewUrlParser:true
-// });
+mongoose.connect('mongodb://localhost/local',{
+  user : 'root',
+  pass : 'liuxiaoma123',
+  // useNewUrlParser:true,
+  useUnifiedTopology: true
+});
 
-mongoose.connect('mongodb://localhost/local');
+// mongoose.connect('mongodb://localhost/local');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
