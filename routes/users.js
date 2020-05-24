5c9fb0ee1cb39e4e5936bfbaf171c8c9f5e22f27
xiaoma <670567137@qq.com>
2020-05-24 20:41:11 +0800

    new

---
+++ routes/users.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+const User = require('../db/model/userModel');
+
+
+router.post('/reg',(req,res)=>{
+    let {name , age , sex} = req.body;
+    User.insertMany({
+        name : name,
+        sex :sex,
+        age : age
+    }).then(()=>{
+        res.send({err : 0 ,msg :  '注册OK'})
+    }).catch((err)=>{
+        res.send({err : -1 , msg : err});
+    })
+})
+
+router.post('/login',(req,res)=>{
+  let {name , age , sex} = req.body;
+  console.log(name)
+  User.find({
+    name : name,
+    // sex :sex,
+    // age : age
+   }).then((result)=>{
+     console.log(result);
+      res.send({err : 1 , msg : '查询OK',data : result})      
+   }).catch((err)=>{
+      res.send({err : 1 , msg : '查询fail',data : err})      
+   })
+})
+
+/* GET users listing. */
+router.get('/:id', function(req, res, next) {
+  let {id} = req.params;
+  console.log(id,req.params);
+  res.render('user', { title: 'Express' });
+});
+
+
+module.exports = router;
