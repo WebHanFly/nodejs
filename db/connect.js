73151cca865c856217145abc7baade60bd3b82fb
xiaoma <670567137@qq.com>
2020-05-24 21:33:04 +0800

    new

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
-let url = 'mongodb://127.0.0.1:27017/local'
-mongoose.connect(url,{
-  user : 'root',
-  pass : 'liuxiaoma123'
-});
+// let url = 'mongodb://127.0.0.1:27017/local'
+// mongoose.connect(url,{
+//   user : 'root',
+//   pass : 'liuxiaoma123',
+//   useNewUrlParser:true
+// });
+
+mongoose.connect('mongodb://localhost/local');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
